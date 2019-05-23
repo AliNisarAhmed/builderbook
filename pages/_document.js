7ab78fbbd51f7ecc34459dab5198962e110c5ea3
AliNisarAhmed <ali.nisar87@gmail.com>
2019-05-23 00:00:14 -0600

    passing initial props from next server

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
+import JssProvider from 'react-jss/lib/JssProvider';
+import getContext from '../lib/context';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   render() {
     return (
       <html lang="en">
@@ -18,7 +21,6 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Muli:300,400:latin"
           />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
           <link
             rel="stylesheet"
             href="https://storage.googleapis.com/builderbook/nprogress.min.css"
@@ -43,10 +45,11 @@ export default class MyDocument extends Document {
                 border-left: 0.25em solid #dfe2e5;
               }
               pre {
-                display: block;
-                overflow-x: auto;
-                padding: 0.5em;
-                background: #FFF;
+                display:block;
+                overflow-x:auto;
+                padding:0.5em;
+                background:#FFF;
+                color: #000;
                 border: 1px solid #ddd;
               }
               code {
@@ -74,3 +77,32 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = ({ renderPage }) => {
+  const pageContext = getContext();
+
+  const page = renderPage((Component) => (props) => (
+    <JssProvider
+      registry={pageContext.sheetsRegistry}
+      generateClassName={pageContext.generateClassName}
+    >
+      <Component pageContext={pageContext} {...props} />
+    </JssProvider>
+  ));
+
+  return {
+    ...page,
+    pageContext,
+    styles: (
+      <style
+        id="jss-server-side"
+        // eslint-disable-next-line
+        dangerouslySetInnerHTML={{
+          __html: pageContext.sheetsRegistry.toString(),
+        }}
+      />
+    ),
+  };
+};
+
+export default MyDocument;
