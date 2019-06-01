a685ff53e9ab75bdfb2ded811f123cd0be0e1d44
AliNisarAhmed <ali.nisar87@gmail.com>
2019-05-31 22:56:11 -0600

    end chap 2

--- server/app.js
+++ server/app.js
@@ -1,18 +1,56 @@
 const express = require('express');
 const next = require('next');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const mongoSessionStore = require('connect-mongo');
+
+const User = require('./models/User');
+
+require('dotenv').config();
 
 const port = process.env.PORT || 8000;
 const ROOT_URL = `http://localhost:${port}`;
 
 const dev = process.env.NODE_ENV !== 'production';
+const MONGO_URL = process.env.MONGO_URL_TEST;
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+};
+
+mongoose.connect(MONGO_URL, options);
+
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
 
-  server.get('/', (req, res) => {
-    const user = { email: 'team@builderbook.org' };
+  const MongoStore = mongoSessionStore(session);
+
+  const sess = {
+    name: 'builderbook.sid',
+    secret: 'HD2w.)q*VqRT4/#NK2M/,E^B)}FED5fWU!dKe[wk',
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 14 * 24 * 60 * 60, // save session 14 days
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+    },
+  };
+
+  server.use(session(sess));
+
+  server.get('/', async (req, res) => {
+    req.session.foo = 'bar';
+    const user = await User.findOne({ slug: 'team-builder-book' });
+    console.log(user);
     app.render(req, res, '/', { user });
   });
 
